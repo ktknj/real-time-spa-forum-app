b5d3c9a041bcf1d8a655bde7f88f38bcfec56fd0
ktknj <task4kj@gmail.com>
2019-04-04 20:48:40 +0900

    model/controller/factoryを作成

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 - Vue.js
 - Vuetify
 - JWT
+
+
+# -rfm　でresource factory model controller を一括作成
+
+php artisan make:model Model/Question -rfm
+php artisan make:model Model/Reply -rfm
+php artisan make:model Model/Category -rfm
+php artisan make:model Model/Like -rfm
+
+<!-- php artisan make:controller ReplyController --api -->
---
+++ database/migrations/2019_04_04_113207_create_replies_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->text('body');
+            $table->integer('question_id')->unsigned();
+
+            $table->integer('user_id');
+            // /外部キー制約
+            // 参考URL：
+            // https://qiita.com/Yorinton/items/3dbf6ad30358bd80fcb0
+            // https://qiita.com/sutara79/items/9e57e7af03b65e0d6047
+            $table->foreign('question_id')->references('id')
+                ->on('questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
