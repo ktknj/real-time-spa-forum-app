df061956b11b554275bb6b5dbe9e7ef1374e254e
ktknj <task4kj@gmail.com>
2019-04-07 10:28:21 +0900

    Question Resoure transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = [
+        'title','slug','body','category_id','user_id'
+    ];
+    // キー名のカスタマイズ
+    // https://readouble.com/laravel/5.8/ja/routing.html
+    // ルーティングにてスラッグでの記述を実現（idの代わりにslugをキーに利用している）
+    // https://qiita.com/netfish/items/4bd83755143bb0d298cb
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function user()
     {
         return $this->belongsTo('App\User');
@@ -19,5 +31,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo('App\Model\Category');
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
